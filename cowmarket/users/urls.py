2e1d97d57c1de0924bc950ef9ab01a02c5e73c9a
jessadakorn65 < jesadakorn.26447@gmail.com>
2025-03-01 02:57:57 +0700

    dashboard

--- cowmarket/users/urls.py
+++ cowmarket/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import login_view, google_login_redirect
 from .views import *  
+from . import views
 
 urlpatterns = [
     path("profile/", profile, name="profile"),
@@ -8,4 +9,7 @@ urlpatterns = [
     path("main/", main_view, name="main"),
     path("login/", login_view, name="login"),  # ✅ หน้า Login
     path("google/login/", google_login_redirect, name="google_login_redirect"),  # ✅ เพิ่ม path ที่หายไป
+    path("profile/", profile, name="profile"),
+    path('cow/<int:cow_id>/', views.cow_detail_view, name='cow_detail'),
 ]
+
